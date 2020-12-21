0af95cf8761cca2d5bb5745c4105b7cbf5645657
LeoIta <leonardolorusso83@gmail.com>
2020-12-21 16:22:38 +0100

    factory beans

--- spring5/src/main/java/guru/springframework/sfgdi/services/GetterGreetingService.java
+++ spring5/src/main/java/guru/springframework/sfgdi/services/GetterGreetingService.java
@@ -3,10 +3,10 @@ package guru.springframework.sfgdi.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ConstructorGreetingService implements GreetingService {
+public class GetterGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - I was injected via the constructor!!!";
+        return "Hello - I was injected by the getter";
     }
 }
